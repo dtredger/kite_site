89e3ba8203a7f8d0cf0c21194d28036f9b22c89e
Dylan Tredger <d.tredger@gmail.com>
2020-12-08 19:43:10 -0500

    add jsonapi-serializer, rack-cors, & devise_token_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,56 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  allow_password_change  :boolean          default(FALSE)
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  locked_at              :datetime
+#  provider               :string           default("email"), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  tokens                 :json
+#  uid                    :string           default(""), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable #,
+         # :confirmable,
+         # :lockable,
+         # :timeoutable,
+         # :trackable,
+         # :omniauthable
+
+  include DeviseTokenAuth::Concerns::User
+
+
+  # https://www.rubydoc.info/github/plataformatec/devise/Devise/Models/Authenticatable
+  # don't worry about confirmable for now
+  # def active_for_authentication?
+  #   true
+  # end
+  # before_save -> do
+  #   skip_confirmation!
+  # end
+
 end
