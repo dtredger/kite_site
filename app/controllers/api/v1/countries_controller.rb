8c0051905432aa1cb2e4d6a441a20e0f3ca25925
Dylan Tredger <d.tredger@gmail.com>
2020-12-28 21:43:33 -0800

    create base_controller for API

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,12 +1,4 @@
-class Api::V1::CountriesController < ActionController::API
-
-  # TODO - create base APIcontroller to include this in all API controllers
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  # https://stackoverflow.com/questions/53699878/gem-rack-cors-not-working-and-request-from-angular-client-to-rails-api-throwin
-  protect_from_forgery with: :null_session
-
-
-  # before_action :authenticate_user!
+class Api::V1::CountriesController < Api::BaseController
 
   # get ?page=2
   def index
--- app/controllers/api/v1/kite_spots_controller.rb
+++ app/controllers/api/v1/kite_spots_controller.rb
@@ -1,10 +1,4 @@
-class Api::V1::KiteSpotsController < ActionController::API
-
-  # TODO - create base APIcontroller to include this in all API controllers
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  # https://stackoverflow.com/questions/53699878/gem-rack-cors-not-working-and-request-from-angular-client-to-rails-api-throwin
-  protect_from_forgery with: :null_session
-
+class Api::V1::KiteSpotsController < Api::BaseController
 
   # get ?page=2
   def index
