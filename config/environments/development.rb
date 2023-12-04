c9dee9e4e7120c9c96735ff0f9e982f55f4f57b1
Dylan Tredger <d.tredger@gmail.com>
2023-12-04 16:02:38 -0500

    typo in config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-<<<<<<< HEAD
-  config.active_storage.service = :local #mirror  #:digital_ocean
-=======
   config.active_storage.service = :local  #:digital_ocean  :local #
->>>>>>> feature/update-ruby
   config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
   # Don't care if the mailer can't send.
