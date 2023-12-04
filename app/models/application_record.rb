5db4254187b803d8a654993f0b7a0d2ca77a5d31
Dylan Tredger <d.tredger@gmail.com>
2023-12-04 15:46:03 -0500

    add to_csv for records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,6 +9,27 @@ class ApplicationRecord < ActiveRecord::Base
     Rails.application.routes.url_helpers.root_url(host: site_host)
   end
 
+  def self.to_csv(filename=nil)
+    require 'csv'
+    attributes = self.attribute_names
+
+    # TODO include associations
+    # self.reflect_on_all_associations.map(&:name)
+    csv = CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.find_each do |model|
+        csv << attributes.map{ |attr| model.send(attr) }
+      end
+    end
+    if filename
+      File.open(filename, 'w') do |file|
+        file.write(csv)
+      end
+      return filename
+    end
+  end
+
+
   def all_months
     %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
   end
