5a12cf86cca4f7bd46013f2a26428f75f37286df
Dylan Tredger <d.tredger@gmail.com>
2023-12-08 19:07:59 -0500

    use local storage for demo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service_urls_expire_in = 1.week
-  config.active_storage.service = :digital_ocean
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
